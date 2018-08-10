f5ea96bc851bdfbb746c036b1dd9696a0def1d2e
jilingjun <jilingjun_jlj@189.cn>
2018-08-10 15:39:22 +0800

    add flannel

--- kubernetes/roles/master/templates/pull_images.sh
+++ kubernetes/roles/master/templates/pull_images.sh
@@ -5,7 +5,7 @@ images=(
   kube-controller-manager-amd64:v1.11.2 
   kube-apiserver-amd64:v1.11.2
   etcd-amd64:3.2.18 
-  pause-amd64:3.1 
+  pause:3.1 
   kubernetes-dashboard-amd64:v1.8.3
   k8s-dns-sidecar-amd64:1.14.8 
   k8s-dns-kube-dns-amd64:1.14.8
